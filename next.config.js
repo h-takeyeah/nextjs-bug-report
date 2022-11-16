66a3f375732bf4d08d47fe46936c2d80e06f7c3c
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-11-17 01:11:12 +0900

    chore(config): write function style

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  swcMinify: true,
-  experimental: {
-    // Required:
-    appDir: true,
-  },
-};
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = nextConfig;
+/**
+ * @param {string} phase 
+ * @param {any} _
+ */
+module.exports = (phase, { defaultConfig }) => {
+  /** @type {import('next').NextConfig} */
+  const sharedConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true
+    }
+  }
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    /** @type {import('next').NextConfig} */
+    const devConfig = {
+      ...sharedConfig
+    }
+    return devConfig
+  } else {
+    /** @type {import('next').NextConfig} */
+    const prodConfig = {
+      ...sharedConfig,
+      output: 'standalone' // turbopack currently does not support `output: 'standalone'`
+    }
+    return prodConfig
+  }
+}
